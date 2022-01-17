c201400f637701903e6e49e5d0799db83a409819
Dennis Korir <dkdenniskorir@outlook.com>
2022-01-17 10:32:20 +0300

    updated the main content
    created the main content for the lesson, added the required styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,9 +2,18 @@ import React from 'react'
 
 function Main() {
     return (
-        <div>
-            <p> Main content goes here</p>
-        </div>
+        <main>
+            <h1 className='main-title'>Fun facts about react</h1>
+            <ul className='main-facts'>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of entreprise apps, including mobile apps</li>
+
+
+            </ul>
+        </main>
     )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,8 @@ function Navbar() {
     return (
         <nav>
             <img src={logo} className="App-logo" alt="logo" />
-            <h3>ReactFacts</h3>
-            <h4> React Course - Project 1</h4>
+            <h3 className="nav-title">ReactFacts</h3>
+            <h4 className="nav-text"> React Course - Project 1</h4>
         </nav>
     )
 }
